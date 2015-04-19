592dc655938d02ded78e053b320b473d27871191
Roman5987 <tuf09209@temple.edu>
2015-04-18 20:11:12 -0400

    functional NFC button

--- app/src/main/java/tk/gonensh/tapin/MainActivity.java
+++ app/src/main/java/tk/gonensh/tapin/MainActivity.java
@@ -1,9 +1,13 @@
 package tk.gonensh.tapin;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import tk.gonensh.tapin.NFCActivity;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button button1 = (Button) findViewById(R.id.NFCbutton);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NFCActivity.class));
+            }
+        });
+
     }
 
 
@@ -36,4 +52,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
